a4b060946e2720a2f6e601b5a7d04d386758bf0f
bhanu-1808 <bhanuprakash4a7@gmail.com>
2025-09-28 11:12:11 +0530

    add p.java file

---
+++ p.java
@@ -0,0 +1,13 @@
+public class p {
+    public static void main (String args[])
+    {
+        int i=8;
+        if(i%2==0)
+        {
+            System.out.println("prime ");
+        }
+        else{
+            System.out.println("not prime");
+        }
+    }
+}
